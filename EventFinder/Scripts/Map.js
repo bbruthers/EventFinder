8a6e1321114b2f3b2d1e2cab043ee15c2dfb0476
brett_bruthers <bbruthers@gmail.com>
2017-05-27 20:57:50 -0400

    only one marker appears on map at any given time.

--- EventFinder/Scripts/Map.js
+++ EventFinder/Scripts/Map.js
@@ -12,26 +12,33 @@
             center: uluru
         });
 
-    var marker = new google.maps.Marker
-        ({
-            position: null,
-            map: map,
-            title: "click!"
-        });
-
     map.addListener('click', function (e)
     {
         placeMarkerAndPanTo(e.latLng, map);
     });
 
+    //https://stackoverflow.com/questions/3684274/googlemaps-v3-api-create-only-1-marker-on-click
+
+    var marker;
+
     function placeMarkerAndPanTo(latLng, map)
     {
-        marker.position = latLng
+        if (marker)
+        {
+            marker.setPosition(latLng);
+        }
+        else
+        {
+            marker = new google.maps.Marker
+            ({
+                position: latLng,
+                map: map
+            });
+
+            map.panTo(marker.position);
+        }
 
         document.getElementById('lat').innerHTML = "Latitude: " + marker.getPosition().lat();
         document.getElementById('lng').innerHTML = "Longitude: " + marker.getPosition().lng();
-
-        marker.setMap(latLng);
-        map.panTo(latLng);
     }
 }
